e410329657eef35903727e3ffbfd6feff6269781
Kevin Crisa <kevin.crisa1@gmail.com>
2026-03-02 21:34:46 +0100

    Add classes for search by increasing cost and ratio volume/cost

---
+++ src/main/kotlin/com/flexcity/assets/fcassets/domain/AssetsSearchByIncreasingCost.kt
@@ -0,0 +1,29 @@
+package com.flexcity.assets.fcassets.domain
+
+class AssetsSearchByIncreasingCost: AssetRequestStrategy {
+    override fun select(
+        assets: List<Asset>,
+        request: AssetRequest
+    ): List<AvailableAsset> {
+        val availableAssetsAtDate = assets.filter { it.availability.contains(request.date) }
+        if (availableAssetsAtDate.isEmpty()) return emptyList()
+
+        val availableAssetsSortByIncreasingCost = availableAssetsAtDate.sortedBy { it.activationCost }
+
+        val assetsSelected = mutableListOf<AvailableAsset>()
+        var requestedVolume = request.volume
+
+        for (asset in availableAssetsSortByIncreasingCost){
+            if(requestedVolume <= 0) break
+            val availableVolume = minOf(asset.volume, requestedVolume)
+            assetsSelected.add(AvailableAsset(asset.code, availableVolume, asset.activationCost))
+            requestedVolume -= availableVolume
+        }
+
+        if (requestedVolume > 0){
+            throw IllegalArgumentException("The volume available in assets are insufficient for requested volume")
+        }
+
+        return assetsSelected
+    }
+}
---
+++ src/main/kotlin/com/flexcity/assets/fcassets/domain/AssetsSearchByRatioVolumeCost.kt
@@ -0,0 +1,28 @@
+package com.flexcity.assets.fcassets.domain
+
+class AssetsSearchByRatioVolumeCost: AssetRequestStrategy {
+    override fun select(
+        assets: List<Asset>,
+        request: AssetRequest
+    ): List<AvailableAsset> {
+        val availableAssetsAtDate = assets.filter { it.availability.contains(request.date) }
+        if (availableAssetsAtDate.isEmpty()) return emptyList()
+
+        val availableAssetsSortByRatioVolumeCost =
+            availableAssetsAtDate.sortedByDescending { it.volume.toDouble() / it.activationCost }
+
+        val assetsSelected = mutableListOf<AvailableAsset>()
+        var requestedVolume = request.volume
+
+        for (asset in availableAssetsSortByRatioVolumeCost){
+            if(requestedVolume <= 0) break
+            assetsSelected.add(AvailableAsset(asset.code, asset.volume, asset.activationCost))
+            requestedVolume -= asset.volume
+        }
+
+        if (requestedVolume > 0){
+            throw IllegalArgumentException("The volume available in assets are insufficient for requested volume")
+        }
+
+        return assetsSelected    }
+}
